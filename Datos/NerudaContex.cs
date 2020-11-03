e1856e590e31d2300283405a88c37adbc423f4bb
laura <lyperea@unicesar.edu.co>
2020-11-02 23:46:42 -0500

    neruda

---
+++ Datos/NerudaContex.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Entidad;
+namespace Datos
+{
+    public class NerudaContext : DbContext
+    {
+        public NerudaContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Habitacion> Habitaciones { get; set; }
+        public DbSet<Reserva> Reservas {get; set;}
+   }
+}
